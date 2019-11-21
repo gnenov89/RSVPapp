55d26f1328855686b414afc6568201b22bc0bc45
gnenov89 <georgi.nenov89@gmail.com>
2019-11-21 16:22:33 -0500

    addind logic to app.js

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const form = document.getElementById('registrar');
 const input = document.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text =input.value;
-    input.value = '';
-    
+function createLI(text){
     const li = document.createElement('li');
     li.textContent = text;
     const label = document.createElement('label');
@@ -15,6 +11,17 @@ form.addEventListener('submit', (e) => {
     checkbox.type = 'checkbox';
     label.appendChild(checkbox);
     li.appendChild(label);
+    const button = document.createElement('button');
+    button.textContent = 'Romove';
+    li.appendChild(button);
+    return li;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = ''; 
+    const li = createLI(text);
     ul.appendChild(li);  
 });
 
@@ -28,6 +35,12 @@ ul.addEventListener('change', (e) => {
     } else {
       listItem.className = '';
      }
-
-
 });
+
+ul.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        const li = e.target.parentNode;
+        const ul = li.parentNode;
+        ul.removeChild(li);
+    }
+})
